e552127ae312702c2d69f979961c098e4b0984dc
Riya Mehere <riyamehere@gmail.com>
2022-02-18 10:27:45 +0530

    4th - 10th exercise done

--- 2. Blackjack game/6. The if...else statement for our game/index.js
+++ 2. Blackjack game/6. The if...else statement for our game/index.js
@@ -1,10 +1,19 @@
 let firstCard = 10
-let secondCard = 7
+let secondCard = 20
 let sum = firstCard + secondCard
 
 // Write the conditional according to these rules:
 
 // if less than or equal to 20 -> "Do you want to draw a new card? 🙂"
+if(sum<=20){
+    console.log("Do you want to draw a new card? 🙂")
+}
 // else if exactly 21 -> "Wohoo! You've got Blackjack! 🥳"
+else if(sum === 21){
+    console.log("Wohoo! You've got Blackjack! 🥳")
+}
 // else -> "You're out of the game! 😭"
+else{
+    console.log("You're out of the game! 😭")
+}
 
